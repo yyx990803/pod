bb578a1080695f7a349a8865b1ca538fb2805f18
Evan <yyou@google.com>
2013-07-12 17:39:08 -0400

    restructure, stub for tests, TODO

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var assert = require('assert'),
+	pod    = require('../lib/pod').initTest()
+
+describe('CLI', function () {
+    it('should pass', function () {
+        assert.equal(1, 1)
+    })
+})
