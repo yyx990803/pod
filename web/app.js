7c0cd310247f362207a6357de1b7149e561ca9fd
Evan You <yyou@google.com>
2013-11-25 21:42:20 -0500

    rename to static

--- web/app.js
+++ web/app.js
@@ -18,7 +18,7 @@ app.configure(function(){
     app.use(express.favicon())
     app.use(reloadConf)
     app.use(app.router)
-    app.use(express.static(path.join(__dirname, 'public')))
+    app.use(express.static(path.join(__dirname, 'static')))
 })
 
 app.get('/', auth, function (req, res) {
